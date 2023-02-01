052ffdbe57cb7bc85322e1aa459f761da6623f1f
Julianne <juliannelicon@mpto.mp.br>
2023-02-01 12:01:52 -0300

    Configura tema

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,14 +1,26 @@
+import 'package:delivery_app/app/core/ui/styles/app_styles.dart';
+import 'package:delivery_app/app/core/ui/styles/colors_app.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
 
   const SplashPage({ super.key });
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Splash'),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Splash'),
+      ),
+      body: Column(
+        children: [
+          Container(),
+          ElevatedButton(onPressed: (){}, child: const Text('teste')),
+          TextFormField(
+            decoration: const InputDecoration(labelText: 'text'),
+          )
+        ],
+      ),
+    );
   }
 }
