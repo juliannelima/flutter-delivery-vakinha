2bc1a4984edd7253674d42167ad74f57431f7e0b
Julianne <juliannelicon@mpto.mp.br>
2023-02-01 15:08:47 -0300

    Refatorando página SplashPage

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -8,22 +8,40 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash'),
-      ),
-      body: Column(
-        children: [
-          Container(),
-          DeliveryButton(
-            label:'texto',
-            onPressed: () {},
-            width: 200,
-            height: 50,
-          ),          
-          TextFormField(
-            decoration: const InputDecoration(labelText: 'text'),
-          )
-        ],
+      body: ColoredBox(
+        color: const Color(0xFF140E0E),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: context.screenWidth,
+                child: Image.asset(
+                  'assets/images/lanche.png', 
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Center(
+              child: Column(
+                children: [
+                  SizedBox(height: context.percentHeight(.30),),
+                  Image.asset('assets/images/logo.png'),
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  DeliveryButton(
+                    width: context.percentWidth(.6),
+                    height: 35,
+                    label: 'ACESSAR', 
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+            
+          ],
+        ),
       ),
     );
   }
