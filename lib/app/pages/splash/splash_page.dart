f138dbd498a649819c9c444db749b3fff7b173ff
Julianne <juliannelicon@mpto.mp.br>
2023-02-01 14:35:52 -0300

    Configura Env e RestClient

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,7 +1,7 @@
-import 'package:delivery_app/app/core/ui/styles/app_styles.dart';
-import 'package:delivery_app/app/core/ui/styles/colors_app.dart';
 import 'package:delivery_app/app/core/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
+
+import '../../core/config/env/env.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
@@ -16,10 +16,11 @@ class SplashPage extends StatelessWidget {
         children: [
           Container(),
           DeliveryButton(
-            label: 'teste',
+            label: Env.i['backend_base_url'] ?? '',
             onPressed: () {},
             width: 200,
             height: 50,
+
           ),
           TextFormField(
             decoration: const InputDecoration(labelText: 'text'),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:delivery_app/app/delivery_app.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'app/core/config/env/env.dart';
+
+void main() async {
+  await Env.i.load();
   runApp(const DeliveryApp());
 }
