38494608c744ead5bd032e70dd0c9690d61ad504
Julianne <juliannelicon@mpto.mp.br>
2023-02-01 13:57:41 -0300

    Adiciona widgets delivery_button

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,21 +1,26 @@
 import 'package:delivery_app/app/core/ui/styles/app_styles.dart';
 import 'package:delivery_app/app/core/ui/styles/colors_app.dart';
+import 'package:delivery_app/app/core/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
-
-  const SplashPage({ super.key });
+  const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Splash'),
       ),
       body: Column(
         children: [
           Container(),
-          ElevatedButton(onPressed: (){}, child: const Text('teste')),
+          DeliveryButton(
+            label: 'teste',
+            onPressed: () {},
+            width: 200,
+            height: 50,
+          ),
           TextFormField(
             decoration: const InputDecoration(labelText: 'text'),
           )
