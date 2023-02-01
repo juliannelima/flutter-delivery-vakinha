12ce53eeb75b29f79b951adb22f155388f36bbba
Julianne <juliannelicon@mpto.mp.br>
2023-02-01 11:07:35 -0300

    Adiciona página splash

---
+++ lib/app/delivery_app.dart
@@ -0,0 +1,17 @@
+import 'package:delivery_app/app/pages/splash/splash_page.dart';
+import 'package:flutter/material.dart';
+
+class DeliveryApp extends StatelessWidget {
+
+  const DeliveryApp({ super.key });
+
+   @override
+   Widget build(BuildContext context) {
+       return MaterialApp(
+        title: 'Delivery App',
+        routes: {
+          '/': (context) => const SplashPage(),
+        },
+       );
+  }
+}
