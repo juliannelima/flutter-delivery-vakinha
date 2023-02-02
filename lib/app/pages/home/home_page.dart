d55a7f85f80b8a6af9046bc6816b878917080aef
Julianne <juliannelicon@mpto.mp.br>
2023-02-02 15:37:12 -0300

    Busca informações da api

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,9 +1,11 @@
-import 'package:delivery_app/app/core/ui/helpers/loader.dart';
-import 'package:delivery_app/app/core/ui/helpers/messages.dart';
 import 'package:delivery_app/app/core/widgets/delivery_appbar.dart';
-import 'package:delivery_app/app/models/product_model.dart';
+import 'package:delivery_app/app/pages/home/home_controller.dart';
+import 'package:delivery_app/app/pages/home/home_state.dart';
 import 'package:delivery_app/app/pages/home/widgets/delivery_product_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../core/ui/base_state/base_state.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,30 +14,48 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with Loader, Messages{
+class _HomePageState extends BaseState<HomePage, HomeController>{
+  @override
+  void onReady() {
+    controller.loadProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: DeliveryAppbar(),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return DeliveryProductTile(
-                  product: ProductModel(
-                    id:0, 
-                    name: 'Lanche X', 
-                    description: 'Lache acompanha pão, hambúguer, mussarela e maionese', 
-                    price: 15.0,
-                    image: 'https://assets.unileversolutions.com/recipes-v2/106684.jpg?imwidth=800'
-                  ),
-                );
-              },
-            )
-          )
-        ],
+      body: BlocConsumer<HomeController, HomeState>(
+        listener: (context, state) {
+          state.status.matchAny(
+            any: () => hideLoader(), 
+            loading: ()=>showLoader(),
+            error: () {
+              hideLoader();
+              showError(state.errorMessage ?? 'Erro não informado');
+            }
+          );
+        },
+        buildWhen: (previous, current) => current.status.matchAny(
+          any: () => false,
+          initial: () => true,
+          loaded: () => true,
+        ),
+        builder: (context, state) {
+          return Column(
+            children: [
+              Expanded(
+                  child: ListView.builder(
+                itemCount: state.products.length,
+                itemBuilder: (context, index) {
+                  final products = state.products[index];
+                  return DeliveryProductTile(
+                    product: products,
+                  );
+                },
+              ))
+            ],
+          );
+        },
       ),
     );
   }
